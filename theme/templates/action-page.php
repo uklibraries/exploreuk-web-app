ab18e3858369bd35faa40a961ae66363c7d12acb
Eric C. Weig <eweig@email.uky.edu>
2018-06-26 14:58:49 -0400

    Consolidate and upgrade Font Awesome.

--- theme/templates/action-page.php
+++ theme/templates/action-page.php
@@ -5,10 +5,10 @@
 <h2><?php echo $r['title_display']; ?></h2>
     <div id="media_player">
 <?php $a = $r['audio']; if (isset($a)): ?>
-<p class="click-to-play-audio" data-id="<?php echo $a['href_id']; ?>" data-href="<?php echo $a['href']; ?>"><span class="icon fa-play"></span><br>play</p>
+<p class="click-to-play-audio" data-id="<?php echo $a['href_id']; ?>" data-href="<?php echo $a['href']; ?>"><i class="fas fa-play"></i><br>play</p>
 <?php endif; ?>
 <?php $v = $r['video']; if (isset($v)): ?>
-<p class="click-to-play-video" data-id="<?php echo $v['href_id']; ?>" data-href="<?php echo $v['href']; ?>"><span class="icon fa-play"></span><br>play</p>
+<p class="click-to-play-video" data-id="<?php echo $v['href_id']; ?>" data-href="<?php echo $v['href']; ?>"><i class="fas fa-play"></i><br>play</p>
 <?php endif; ?>
     </div>
 <?php endif; ?>
