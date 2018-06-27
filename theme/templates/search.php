4a673fd7c23959107d808ed44c3998c141b85830
Eric C. Weig <eweig@email.uky.edu>
2018-06-27 14:12:44 -0400

    [partial] Add openable side menu.

--- theme/templates/search.php
+++ theme/templates/search.php
@@ -3,6 +3,8 @@
         <input class="q form-control" type="text" name="q" value="<?php echo q('q'); ?>">
         <span class="input-group-btn"><button type="submit" class="btn btn-default" value="search">Search</button></span>
         &nbsp;<span  data-tooltip="Search for a specific phrase by surrounding it in quotation marks."><i class="fas fa-info-circle" id="app"></i></span>
+
+    <div id="browse_facet_top_page_button"><span id="browse_facet_label"><?php echo get_theme_option('Browse Content Button Text'); ?></span></div>
     <div id="featured_collections"><span id="featured_collections_box"><div id="featured_collections_content">Featured Collections:<ul id="featured_collections_text">
 <?php
 foreach ($featured_collections as $s_id => $s_label):
@@ -21,32 +23,37 @@ endforeach;
 
     </div>
 </form>
-<article>
-<details>
-<summary><?php echo get_theme_option('Refine Search Text'); ?></summary>
+
+    <div id="mySidenav" class="sidenav">
+  <span class="closebtn">&times;</span>
+<div id="facet_group">
+<div id="facet_group_left">
+
 <?php if (count(m('active_facets')) > 0): ?>
 <div id="active_facets" class="row">
 <?php foreach (m('active_facets') as $active): ?>
 <article class="4u 12u$(mobile)">
-<h3><?php echo $active['field_label']; ?></h3>
+<details><summary><?php echo $active['field_label']; ?></summary><br/>
 <ul>
 <li><?php echo $active['value_label']; ?> (<?php echo $active['count']; ?>) <a href="<?php echo $active['remove_link']; ?>">[remove]</a></li>
-</ul>
+</ul></details>
 </article>
 <?php endforeach; ?>
 </div>
 <?php endif; ?>
 <div id="facets" class="row">
 <?php foreach (m('facets') as $facet): ?>
-<article class="4u 12u$(mobile)">
-<h3><?php echo $facet['field_label']; ?></h3>
+<article class="4u 12u$(mobile)"><details><summary>
+<?php echo $facet['field_label']; ?></summary><br/>
 <ul>
 <?php foreach ($facet['values'] as $value): ?>
 <li><a href="<?php echo $value['add_link']; ?>"><?php echo $value['value_label']; ?> (<?php echo $value['count']; ?>)</a></li>
 <?php endforeach; ?>
-</ul>
+</ul></details>
 </article>
 <?php endforeach; ?>
 </div>
-</details>
-</article>
+
+</div>
+</div>
+</div>
