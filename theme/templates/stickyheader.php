dd8e8c816546651af0636424d5b87f22dc7ee4d5
MLE Slone <m.slone@gmail.com>
2018-06-27 16:19:21 -0400

    Fix UK link URL.

--- theme/templates/stickyheader.php
+++ theme/templates/stickyheader.php
@@ -1,5 +1,5 @@
 <header class="sticky">
-<div class="bg-uklblack" id="ukhomelink"><a href="http:/www.uky.edu/UKHome/">UK HOME</a></div>
+<div class="bg-uklblack" id="ukhomelink"><a href="https://www.uky.edu/">UK HOME</a></div>
 <div class="row bg-uklblue">
     <div>
 <?php require_once("$base_dir/templates/sitetitle.php"); ?>
