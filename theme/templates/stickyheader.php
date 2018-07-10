6dc06357990c17418ec0166dfe09484009df133a
Michael Slone <m.slone@gmail.com>
2018-07-10 10:09:48 -0400

    Add division and college links to header.

--- theme/templates/stickyheader.php
+++ theme/templates/stickyheader.php
@@ -1,5 +1,11 @@
 <header class="sticky">
-<div class="bg-uklblack" id="ukhomelink"><a href="https://www.uky.edu/UKHome/">UK HOME</a></div>
+<div class="bg-uklblack" id="ukhomelink">
+    <span class="toplinks">
+    <a href="https://www.uky.edu/UKHome/" target="_blank">University of Kentucky</a> |
+    <a href="https://libraries.uky.edu/" target="_blank">UK Libraries</a> |
+    <a href="https://libraries.uky.edu/SC" target="_blank">Special Collections Research Center</a>
+    </span>
+</div>
 <div class="row bg-uklblue">
     <div>
 <?php require_once("$base_dir/templates/sitetitle.php"); ?>
