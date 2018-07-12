fad1b7e466163f4a39044d15122d87c31aaaab0a
Michael Slone <m.slone@gmail.com>
2018-07-12 13:47:07 -0400

    Move front page styling to front page template.

---
+++ theme/templates/front-page.php
@@ -0,0 +1,9 @@
+<style type="text/css">
+#top > div {
+    background-image:url(<?php echo $featured_image['background-image']; ?>);
+    height: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+</style>
