c6a4aac1cc2dcd0a3476a62a7e6d0f8b9dfb8c5e
Michael Slone <m.slone@gmail.com>
2018-07-18 10:02:38 -0400

    Remove facets menu from front page. Also remove some unnecessary CSS and JavaScript.

--- theme/templates/search.php
+++ theme/templates/search.php
@@ -9,37 +9,3 @@
 
     </div>
 </form>
-
-    <div id="mySidenav" class="sidenav">
-  <span class="closebtn">&times;</span>
-<div id="facet_group">
-<div id="facet_group_left">
-
-<?php if (count(m('active_facets')) > 0): ?>
-<div id="active_facets" class="row">
-<?php foreach (m('active_facets') as $active): ?>
-<article class="4u 12u$(mobile)">
-<details><summary><?php echo $active['field_label']; ?></summary><br/>
-<ul>
-<li><?php echo $active['value_label']; ?> (<?php echo $active['count']; ?>) <a href="<?php echo $active['remove_link']; ?>">[remove]</a></li>
-</ul></details>
-</article>
-<?php endforeach; ?>
-</div>
-<?php endif; ?>
-<div id="facets" class="row">
-<?php foreach (m('facets') as $facet): ?>
-<article class="4u 12u$(mobile)"><details><summary>
-<?php echo $facet['field_label']; ?></summary><br/>
-<ul>
-<?php foreach ($facet['values'] as $value): ?>
-<li><a href="<?php echo $value['add_link']; ?>"><?php echo $value['value_label']; ?> (<?php echo $value['count']; ?>)</a></li>
-<?php endforeach; ?>
-</ul></details>
-</article>
-<?php endforeach; ?>
-</div>
-
-</div>
-</div>
-</div>
