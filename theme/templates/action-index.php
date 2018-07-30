e7028d7ca091ef94e1fa46d6e7c7d452721fce2f
MLE Slone <m.slone@gmail.com>
2018-07-30 15:38:54 -0400

    Adjust layout for search results.

--- theme/templates/action-index.php
+++ theme/templates/action-index.php
@@ -71,20 +71,25 @@ $p = m('pagination');
 <?php foreach (m('results') as $r): ?>
 <li class="result-item">
     <p class="result-number"><?php echo $r['number']; ?>.</p>
-    <?php if (isset($r['thumb'])): ?>
-    <a class="image-placeholder" href="<?php echo $r['link']; ?>">
-        <img src="<?php echo $r['thumb']; ?>" title="<?php echo $r['title']; ?>">
-    </a>
-    <?php else: ?>
-    <div class="image-placeholder"></div>
-    <?php endif; ?>
     <div class="result-summary">
         <h3><a href="<?php echo $r['link']; ?>"><?php echo euk_brevity($r['title']); ?></a></h3>
+        <?php if (isset($r['thumb'])): ?>
+        <a class="image-placeholder" href="<?php echo $r['link']; ?>">
+            <img src="<?php echo $r['thumb']; ?>" title="<?php echo $r['title']; ?>">
+        </a>
+        <?php else: ?>
+        <div class="image-placeholder"></div>
+        <?php endif; ?>
         <ul class="result-metadata">
             <?php foreach ($euk_result_facet_order as $field): ?>
                 <?php if (isset($r[$field])): ?>
                     <?php $label = $euk_locale['en'][$hit_fields[$field]]; ?>
-                    <li><span class="result-metadata-label"><?php echo $label; ?>:</span>
+                    <?php if (in_array($field, $euk_result_drop_fields)): ?>
+                        <?php $lclass = ' class="result-metadata-drop"'; ?>
+                    <?php else: ?>
+                        <?php $lclass = ''; ?>
+                    <?php endif; ?>
+                    <li<?php echo $lclass; ?>><span class="result-metadata-label"><?php echo $label; ?>:</span>
                     <?php echo $r[$field]; ?></li>
                 <?php endif; ?>
             <?php endforeach; ?>
