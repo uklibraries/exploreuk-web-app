39b29da289c41a0ac5f33fa4409058d2792dec7d
MLE Slone <m.slone@gmail.com>
2018-08-01 10:19:24 -0400

    Use buttons for click-to-play audiovisual media.

--- theme/templates/audio-video-player.php
+++ theme/templates/audio-video-player.php
@@ -1,10 +1,10 @@
 <?php $r = m('item_audio'); ?>
 <div id="media_player">
     <?php $a = $r['audio']; if (isset($a)): ?>
-    <p class="click-to-play-audio" data-id="<?php echo $a['href_id']; ?>" data-href="<?php echo $a['href']; ?>"><i class="fas fa-play"></i><br>play</p>
+    <button class="click-to-play-audio" data-id="<?php echo $a['href_id']; ?>" data-href="<?php echo $a['href']; ?>"><i class="fas fa-play"></i><br>play</button>
     <?php endif; ?>
 
     <?php $v = $r['video']; if (isset($v)): ?>
-    <p class="click-to-play-video" data-id="<?php echo $v['href_id']; ?>" data-href="<?php echo $v['href']; ?>"><i class="fas fa-play"></i><br>play</p>
+    <button class="click-to-play-video" data-id="<?php echo $v['href_id']; ?>" data-href="<?php echo $v['href']; ?>"><i class="fas fa-play"></i><br>play</button>
     <?php endif; ?>
 </div>
