f5cda306a15962a8b2f04f6863c5a85f37b641fa
Michael Slone <m.slone@gmail.com>
2018-08-01 10:43:28 -0400

    Add stub HTML for details tabbing.

--- theme/templates/page-details.php
+++ theme/templates/page-details.php
@@ -1,4 +1,11 @@
-<div class="item-details">
+<ul>
+<li><a href="#item-details">Item Details</a></li>
+<?php if (m('item_book')): ?>
+<li><a href="#item-text">Text</a></li>
+<?php endif; ?>
+</ul>
+
+<div id="item-details" class="item-details">
 <?php foreach ($euk_detail_field_order as $field):
         $content = meta($field);
         if ($content):
@@ -9,7 +16,7 @@
 
 <?php if (m('item_book')): ?>
 <?php $r = m('item_book')['text']; ?>
-<div class="item-text">
+<div id="item-text" class="item-text">
     <p>The text may or may not be an accurate representation of the original.</p>
     <iframe id="text_frame" src="<?php echo $r['href']; ?>" width="100%" name="text"></iframe>
 </div>
