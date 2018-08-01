420eb35c279c89e42eab0cf1a69ca7912dca8da3
Michael Slone <m.slone@gmail.com>
2018-08-01 16:12:37 -0400

    Make audio/video controls mimic download controls.

--- theme/templates/audio-video-player.php
+++ theme/templates/audio-video-player.php
@@ -1,10 +1,10 @@
 <?php $r = m('item_audio'); ?>
-<div id="media_player">
+<ul class="media-player">
     <?php $a = $r['audio']; if (isset($a)): ?>
-    <button class="click-to-play-audio" data-id="<?php echo $a['href_id']; ?>" data-href="<?php echo $a['href']; ?>"><i class="fas fa-play"></i><br>play</button>
+    <li class="bg-uklblack"><a class="click-to-play-audio" data-id="<?php echo $a['href_id']; ?>" data-href="<?php echo $a['href']; ?>"><i class="fas fa-play"></i><p>Play Audio</p></a></li>
     <?php endif; ?>
 
     <?php $v = $r['video']; if (isset($v)): ?>
-    <button class="click-to-play-video" data-id="<?php echo $v['href_id']; ?>" data-href="<?php echo $v['href']; ?>"><i class="fas fa-play"></i><br>play</button>
+    <li class="bg-uklblack"><a class="click-to-play-video" data-id="<?php echo $v['href_id']; ?>" data-href="<?php echo $v['href']; ?>"><i class="fas fa-play"></i><p>Play Video</p></a></li>
     <?php endif; ?>
-</div>
+</ul>
