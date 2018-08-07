f1c65fbe5abc32144d41da8e2e2a5e3859a6005d
MLE Slone <m.slone@gmail.com>
2018-08-07 12:35:21 -0400

    Fix typo.

--- theme/common/footer.php
+++ theme/common/footer.php
@@ -14,26 +14,26 @@ require_once("$base_dir/euk/euk.php");
     <div id="outer_footer_funders" class="row bg-uklwhite">
         <span>Content made available with the support of:</span>
         <ul id="inner_footer_funders">
-            <li><a href="https://www.imls.gov/" target="_blank" rel="nooopener"><img id="footer_funder_image" class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/imls-n.png" alt="Institute of Museum and Library Services" width="231" height="83"/></a></li>
-            <li><a href="https://www.clir.org/" target="_blank" rel="nooopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/clir-n.png" alt="Council on Library and Information Resources" width="167" height="76"/></a></li>
-            <li><a href="https://www.neh.gov/" target="_blank" rel="nooopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/neh-n.jpg" alt="National Endowment for the Humanities" width="214" height="99"/></a></li>
-            <li><a href="https://www.archives.gov/nhprc" target="_blank" rel="nooopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/nhprc-n.jpg" alt="National Archives - National Historical Publications and Records Commission" width="117" height="147"/></a></li>
-            <li><a href="https://libraries.uky.edu/page.php?lweb_id=1114" target="_blank" rel="nooopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/heyburn-n.png" alt="The John G. Heyburn II Initiative for Excellence in the Federal Judiciary" width="118" height="89"/></a></li>
+            <li><a href="https://www.imls.gov/" target="_blank" rel="noopener"><img id="footer_funder_image" class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/imls-n.png" alt="Institute of Museum and Library Services" width="231" height="83"/></a></li>
+            <li><a href="https://www.clir.org/" target="_blank" rel="noopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/clir-n.png" alt="Council on Library and Information Resources" width="167" height="76"/></a></li>
+            <li><a href="https://www.neh.gov/" target="_blank" rel="noopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/neh-n.jpg" alt="National Endowment for the Humanities" width="214" height="99"/></a></li>
+            <li><a href="https://www.archives.gov/nhprc" target="_blank" rel="noopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/nhprc-n.jpg" alt="National Archives - National Historical Publications and Records Commission" width="117" height="147"/></a></li>
+            <li><a href="https://libraries.uky.edu/page.php?lweb_id=1114" target="_blank" rel="noopener"><img class="lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/heyburn-n.png" alt="The John G. Heyburn II Initiative for Excellence in the Federal Judiciary" width="118" height="89"/></a></li>
         </ul>
     </div>
     <div id="copyright-bar" class="row bg-uklblack">
         <span id="copyright">Copyright © 2018 UK Libraries. For questions or comments about this website, contact <a href="mailto:sarah.dorpinghaus@uky.edu">Sarah Dorpinghaus</a>.</span>
         <ul>
-            <li><a aria-label="Special Collections Research Center on Facebook" href="https://www.facebook.com/ukscrc/" target="_blank" rel="nooopener"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a aria-label="University of Kentucky Libraries on Twitter" href="https://twitter.com/UKLibraries" target="_blank" rel="nooopener"><i class="fab fa-twitter"></i></a></li>
-            <li><a aria-label="University of Kentucky Libraries on Instagram" href="https://www.instagram.com/uklibraries/" target="_blank" rel="nooopener"><i class="fab fa-instagram"></i></a></li>
-            <li><a aria-label="University of Kentucky Archives on YouTube" href="https://www.youtube.com/channel/UCxgb2cJ2hpO-0QOTnmxgfKw" target="_blank" rel="nooopener"><i class="fab fa-youtube"></i></a></li>
-            <li><a aria-label="Curiosities and Wonders" href="https://ukyarchives.blogspot.com/" target="_blank" rel="nooopener"><i class="fab fa-blogger"></i></a></li>
+            <li><a aria-label="Special Collections Research Center on Facebook" href="https://www.facebook.com/ukscrc/" target="_blank" rel="noopener"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a aria-label="University of Kentucky Libraries on Twitter" href="https://twitter.com/UKLibraries" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a></li>
+            <li><a aria-label="University of Kentucky Libraries on Instagram" href="https://www.instagram.com/uklibraries/" target="_blank" rel="noopener"><i class="fab fa-instagram"></i></a></li>
+            <li><a aria-label="University of Kentucky Archives on YouTube" href="https://www.youtube.com/channel/UCxgb2cJ2hpO-0QOTnmxgfKw" target="_blank" rel="noopener"><i class="fab fa-youtube"></i></a></li>
+            <li><a aria-label="Curiosities and Wonders" href="https://ukyarchives.blogspot.com/" target="_blank" rel="noopener"><i class="fab fa-blogger"></i></a></li>
         </ul>
         <ul>
-            <li><a href="https://libraries.uky.edu/SC" target="_blank" rel="nooopener"><img class="euk-logo-small lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/scrc_logo.png" alt="UK Special Collections Research Center" width="134" height="34"/></a></li>
-            <li><a href="https://www.gpo.gov/" target="_blank" rel="nooopener"><img class="euk-logo-small lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/gpo_fst.jpg" alt="US Government Publishing Office" width="74" height="34"/></a></li>
-            <li><a href="https://www.fdlp.gov/" target="_blank" rel="nooopener"><img class="euk-logo-small lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/fdlp.png" alt="Federal Depository Library Program" width="46" height="34"/></a></li>
+            <li><a href="https://libraries.uky.edu/SC" target="_blank" rel="noopener"><img class="euk-logo-small lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/scrc_logo.png" alt="UK Special Collections Research Center" width="134" height="34"/></a></li>
+            <li><a href="https://www.gpo.gov/" target="_blank" rel="noopener"><img class="euk-logo-small lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/gpo_fst.jpg" alt="US Government Publishing Office" width="74" height="34"/></a></li>
+            <li><a href="https://www.fdlp.gov/" target="_blank" rel="noopener"><img class="euk-logo-small lazy" src="<?php echo $theme_path; ?>/images/middlegray.png" data-src="<?php echo $theme_path; ?>/images/fdlp.png" alt="Federal Depository Library Program" width="46" height="34"/></a></li>
         </ul>
     </div>
 </div>
