26522fd7bdc88705f3e20ca58f338cf92426e2da
Michael Slone <m.slone@gmail.com>
2018-08-08 15:25:59 -0400

    Update layout for nav menu.

--- theme/templates/nav.php
+++ theme/templates/nav.php
@@ -1,6 +1,5 @@
-<div id="cd-nav">
-<a href="#0" class="cd-nav-trigger">Menu<span></span></a>
-<nav id="cd-main-nav">
-<?php echo public_nav_main(); ?>
+<nav>
+    <label for="drop" class="toggle">Menu</label>
+    <input type="checkbox" id="drop" />
+    <?php echo public_nav_main(); ?>
 </nav>
-</div>
