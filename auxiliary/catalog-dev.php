b772dc234b7b9a30a9b646aa5edf161860e031ed
Michael Slone <m.slone@gmail.com>
2018-08-15 12:58:32 -0400

    Drop support for deployment with Basic Auth.

--- auxiliary/catalog-dev.php
+++
@@ -1,9 +0,0 @@
-$username = 'okapi';
-$home = posix_getpwuid(posix_getuid())['dir'];
-$password = trim(file_get_contents("$home/okapi/okapi.txt"));
-$context = stream_context_create(array(
-    'http' => array(
-        'header' => 'Authorization: Basic ' . base64_encode("$username:$password")
-    )
-));
-print file_get_contents($dest, false, $context);
--- auxiliary/catalog-prod.php
+++
@@ -1,1 +0,0 @@
-print file_get_contents($dest);
