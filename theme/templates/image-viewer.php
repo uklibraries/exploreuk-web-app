fb189bb8e366e6b052b6b6c4da781cad23fd7bec
Michael Slone <m.slone@gmail.com>
2018-08-16 09:32:58 -0400

    Add alt, title attributes to some images.

--- theme/templates/image-viewer.php
+++ theme/templates/image-viewer.php
@@ -2,19 +2,19 @@
 
 <?php if (m('action') === 'zoom'): ?>
 <div id="image_viewer">
-    <a href="<?php echo $r['reference_image_url_s']; ?>"><img class="reference_image" id="reference_image" src="<?php echo $r['front_thumbnail_url_s']; ?>"></a>
+    <a href="<?php echo $r['reference_image_url_s']; ?>"><img class="reference_image" id="reference_image" src="<?php echo $r['front_thumbnail_url_s']; ?>" alt="<?php echo $r['title_display']; ?>" title="<?php echo $r['title_display']; ?>"></a>
     <div id="viewer" class="full-viewport"></div>
 </div>
 <?php else: ?>
     <?php $ui = q('ui'); if (!isset($ui) || !$ui) { $ui = "1"; }?>
     <?php if ($ui === "1"): ?>
 <div id="image_viewer">
-    <a href="<?php echo $r['reference_image_url_s']; ?>"><img class="reference_image" id="reference_image" src="<?php echo $r['front_thumbnail_url_s']; ?>"></a>
+    <a href="<?php echo $r['reference_image_url_s']; ?>"><img class="reference_image" id="reference_image" src="<?php echo $r['front_thumbnail_url_s']; ?>" alt="<?php echo $r['title_display']; ?>" title="<?php echo $r['title_display']; ?>"></a>
     <div id="viewer" style="width: 100%; height: 600px;"></div>
 </div>
     <?php elseif ($ui === "2"): ?>
 <div id="image_viewer">
-    <a href="<?php echo $r['reference_image_url_s']; ?>" target="_blank" rel="noopener"><img class="reference_image static-thumb" id="reference_image" src="<?php echo $r['front_thumbnail_url_s']; ?>" srcset="<?php echo $r['front_thumbnail_url_s']; ?> 400w, <?php echo $r['reference_image_url_s']; ?> <?php echo $r['reference_image_width_s']; ?>w" sizes="(max-width: 800px) 400px, <?php echo $r['reference_image_width_s']; ?>px"></a>
+    <a href="<?php echo $r['reference_image_url_s']; ?>" target="_blank" rel="noopener"><img class="reference_image static-thumb" id="reference_image" src="<?php echo $r['front_thumbnail_url_s']; ?>" srcset="<?php echo $r['front_thumbnail_url_s']; ?> 400w, <?php echo $r['reference_image_url_s']; ?> <?php echo $r['reference_image_width_s']; ?>w" sizes="(max-width: 800px) 400px, <?php echo $r['reference_image_width_s']; ?>px" alt="<?php echo $r['title_display']; ?>" title="<?php echo $r['title_display']; ?>"></a>
 </div>
     <?php endif; ?>
 <?php endif; ?>
