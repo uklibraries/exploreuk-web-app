f6d2ae12e52dc80b0b9499f548da6f6a2c58ba3a
Eric C. Weig <eweig@email.uky.edu>
2018-08-16 12:49:51 -0400

    Update page-details.php

--- theme/templates/page-details.php
+++ theme/templates/page-details.php
@@ -1,9 +1,12 @@
-<ul id="item-view-tabs">
+<div class="tab-wrap">
+<div class="tab-nav">
+<ul id="item-view-tabs" class="tabs">
 <li id="item-view-details" class="active"><a href="#item-details">Item Details</a></li>
 <?php if (m('item_book')): ?>
 <li id="item-view-text"><a href="#item-text">Text</a></li>
 <?php endif; ?>
 </ul>
+</div>
 
 <div id="item-details" class="tab-content active">
 <?php foreach ($euk_detail_field_order as $field):
@@ -19,5 +22,6 @@
 <div id="item-text" class="tab-content hide">
     <p>The text may or may not be an accurate representation of the original.</p>
     <iframe id="text_frame" src="<?php echo $r['href']; ?>" width="100%" name="text"></iframe>
+</div>
 </div>
 <?php endif; ?>
