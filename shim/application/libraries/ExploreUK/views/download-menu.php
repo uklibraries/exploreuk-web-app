540a8deaa54d4d09631f57698efe7763932bbdbe
Michael Slone <m.slone@gmail.com>
2018-08-27 15:10:05 -0400

    Clarify language for paged item download buttons.

--- shim/application/libraries/ExploreUK/views/download-menu.php
+++ shim/application/libraries/ExploreUK/views/download-menu.php
@@ -1,4 +1,8 @@
 <ul class="download">
-    <li class="bg-uklblack"><a id="pdf_href" href="<?= $this->path('/catalog/' . $m['id'] . '/download?type=pdf') ?>"><i class="fas fa-file-pdf"></i><p>Download PDF</p></a></li>
-    <li class="bg-uklblack"><a id="jpeg_href" href="<?= $this->path('/catalog/' . $m['id'] . '/download?type=jpeg') ?>"><i class="fas fa-file-image"></i><p>Download JPEG</p></a></li>
+    <li class="bg-uklblack"><a id="pdf_href" href="<?= $this->path('/catalog/' . $m['id'] . '/download?type=pdf') ?>"><i class="fas fa-file-pdf"></i><p>Download PDF<?= isset($m['downloadable_extra']) ? $m['downloadable_extra'] : '' ?></p></a></li>
+    <li class="bg-uklblack"><a id="jpeg_href" href="<?= $this->path('/catalog/' . $m['id'] . '/download?type=jpeg') ?>"><i class="fas fa-file-image"></i><p>Download JPEG<?= isset($m['downloadable_extra']) ? $m['downloadable_extra'] : '' ?></p></a></li>
+<?php if (isset($m['downloadable_extra'])) : ?>
+    <hr>
+    <li class="download-contact-offer">Want to download entire<br>item/folder? <a href="https://libraries.uky.edu/ContactSCRC" target="_blank" rel="noopener">Contact us</a></li>
+<?php endif; ?>
 </ul>
