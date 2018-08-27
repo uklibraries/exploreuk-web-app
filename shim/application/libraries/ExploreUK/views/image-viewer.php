8e691aa9d20cc49877d23aee502f9a66d5b7902d
MLE Slone <m.slone@gmail.com>
2018-08-27 10:51:45 -0400

    Add label to image links on item page.

--- shim/application/libraries/ExploreUK/views/image-viewer.php
+++ shim/application/libraries/ExploreUK/views/image-viewer.php
@@ -2,7 +2,7 @@
 
 <?php if ($m['action'] === 'zoom') : ?>
 <div id="image_viewer">
-    <a href="<?= $r['reference_image_url_s'] ?>"><img class="reference_image" id="reference_image" src="<?= $r['front_thumbnail_url_s'] ?>" alt="<?= $r['title_display'] ?>" title="<?= $r['title_display'] ?>"></a>
+    <a href="<?= $r['reference_image_url_s'] ?>" aria-label="<?= $r['title_display'] ?>"><img class="reference_image" id="reference_image" src="<?= $r['front_thumbnail_url_s'] ?>" alt="<?= $r['title_display'] ?>" title="<?= $r['title_display'] ?>"></a>
     <div id="viewer" class="full-viewport"></div>
 </div>
 <?php else : ?>
@@ -13,7 +13,7 @@
     }
     if ($ui === "1") : ?>
 <div id="image_viewer">
-    <a href="<?= $r['reference_image_url_s'] ?>"><img class="reference_image" id="reference_image" src="<?= $r['front_thumbnail_url_s'] ?>" alt="<?= $r['title_display'] ?>" title="<?= $r['title_display'] ?>"></a>
+    <a href="<?= $r['reference_image_url_s'] ?>" aria-label="<?= $r['title_display'] ?>"><img class="reference_image" id="reference_image" src="<?= $r['front_thumbnail_url_s'] ?>" alt="<?= $r['title_display'] ?>" title="<?= $r['title_display'] ?>"></a>
     <div id="viewer" style="width: 100%; height: 600px;"></div>
 </div>
     <?php elseif ($ui === "2") : ?>
