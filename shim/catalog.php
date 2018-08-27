edd5f0add2ce073fbb69faf9ef3a4db431146704
MLE Slone <m.slone@gmail.com>
2018-08-27 09:55:43 -0400

    Remove definitions from catalog.php.

--- shim/catalog.php
+++ shim/catalog.php
@@ -1,7 +1,4 @@
 <?php
-if (!defined('EUK_BASE_DIR')) {
-    define('EUK_BASE_DIR', dirname(__FILE__));
-}
 require_once('application/libraries/ExploreUK/init.php');
 $app = new ExploreUK\ExploreUK();
 $app->run();
