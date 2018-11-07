5d306870b9213acf582881eb300f1aa25464da56
MLE Slone <m.slone@gmail.com>
2018-11-07 12:49:32 -0500

    Add favicon.

--- shim/application/libraries/ExploreUK/views/header.php
+++ shim/application/libraries/ExploreUK/views/header.php
@@ -5,7 +5,14 @@
     <meta charset="utf-8" />
     <meta name="description" content="<?= $m['page_description'] ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png?v=m2LeKYRNPO">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=m2LeKYRNPO">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=m2LeKYRNPO">
+    <link rel="manifest" href="/site.webmanifest?v=m2LeKYRNPO">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=m2LeKYRNPO" color="#005dab">
+    <link rel="shortcut icon" href="/favicon.ico?v=m2LeKYRNPO">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#005dab">
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23584444-4"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
