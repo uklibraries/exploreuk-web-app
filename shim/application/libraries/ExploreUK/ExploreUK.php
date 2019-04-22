698ac392e4c5d31735b09413a89ba753b02f58b5
MLE Slone <m.slone@gmail.com>
2019-04-22 09:09:00 -0400

    Remove quotes from queries sent to BookReader.

--- shim/application/libraries/ExploreUK/ExploreUK.php
+++ shim/application/libraries/ExploreUK/ExploreUK.php
@@ -318,11 +318,13 @@ class ExploreUK
             $search_host = 'https://' . $_SERVER['HTTP_HOST'] . '/catalog/' . $parent . '/find';
             $images_base_url = 'https://' . $_SERVER['HTTP_HOST'] . '/themes/' . $metadata['theme'] . '/BookReader/images/';
 
+            $dequoted_query = str_replace('"', '', $metadata['query']->q('q'));
+
             $metadata['script'] = array(
                 'json' => json_encode($pages),
                 'search_host' => json_encode($search_host),
                 'imagesBaseURL' => json_encode($images_base_url),
-                'query' => json_encode($metadata['query']->q('q')),
+                'query' => json_encode($dequoted_query),
             );
         }
 
