9dfb8e3f4b60976bca150c45767177d85aa54db8
MLE Slone <m.slone@gmail.com>
2019-04-22 08:37:21 -0400

    Quote search query.

--- shim/application/libraries/ExploreUK/views/search-brief.php
+++ shim/application/libraries/ExploreUK/views/search-brief.php
@@ -1,6 +1,6 @@
 <form action="<?= $this->path('/catalog/') ?>" method="get" id="search-brief">
     <div class="bg-uklblack form-group-brief">
-        <input aria-label="Search" class="q form-control" type="text" name="q" value="<?= $this->q('q') ?>">
+        <input aria-label="Search" class="q form-control" type="text" name="q" value="<?= htmlspecialchars($this->q('q')) ?>">
         <span class="input-group-btn"></span><button type="submit" class="btn btn-default" value="search">Search</button>
     </div>
 <?php foreach ($this->hiddenSearchFields() as $field) : ?>
