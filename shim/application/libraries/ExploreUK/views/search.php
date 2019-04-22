27ec2ff26efd93cd5886900811e9af11f426dc6d
Michael Slone <m.slone@gmail.com>
2019-04-22 08:37:21 -0400

    Quote search query.

--- shim/application/libraries/ExploreUK/views/search.php
+++ shim/application/libraries/ExploreUK/views/search.php
@@ -1,7 +1,7 @@
 <form action="<?= $this->path('/catalog/') ?>" method="get" id="search">
     <div class="form-group">
         <span><?= $m['search_items_count_text'] ?></span>
-        <span><input aria-label="Search" class="q form-control" type="text" name="q" value="<?= $this->q('q') ?>">
+        <span><input aria-label="Search" class="q form-control" type="text" name="q" value="<?= htmlspecialchars($this->q('q')) ?>">
         <span class="input-group-btn"><button type="submit" class="btn btn-default bg-uklblue" value="search">Search</button></span></span>
     </div>
 
