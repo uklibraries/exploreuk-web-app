8077b46e9cced841ddfa6b9ae98e9348316d518a
MLE Slone <m.slone@gmail.com>
2020-09-21 15:03:14 -0400

    Add harmful language statement.

---
+++ shim/application/libraries/ExploreUK/views/page-harmful-language-statement.php
@@ -0,0 +1,10 @@
+<div class="page-harmful-language-statement bg-uklsky">
+
+<p>
+You may come across language in UK Libraries Special Collections Research Center collections and online resources that you find harmful or offensive. SCRC collects materials from different cultures and time periods to preserve and make available the historical record. These materials document the time period when they were created and the view of their creator. As a result, some may demonstrate racist and offensive views that do not reflect the values of UK Libraries.
+</p>
+
+<p>
+If you find description with problematic language that you think SCRC should review, please contact us at <a href="mailto:SCRC@uky.edu">SCRC@uky.edu</a>.
+</p>
+</div>
