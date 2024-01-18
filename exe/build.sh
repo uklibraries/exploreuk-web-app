342217abac4337da426883e773ce7719ef0ba8f7
MLE Slone <m.slone@gmail.com>
2024-01-18 13:09:15 -0500

    Reduce permissions for files in build artifact.

--- exe/build.sh
+++ exe/build.sh
@@ -11,6 +11,8 @@ rsync -crlpt theme/ "dist/pack/themes/$THEME/"
 cd dist/pack
 find . -type f -name "*.swp" | xargs -n 1 --no-run-if-empty rm
 rm "themes/$THEME/assets/css/main.css"
+find . -type d -exec chmod 0755 {} \;
+find . -type f -exec chmod 0644 {} \;
 tar zcf ../omeuka.tar.gz .
 cd ../..
 echo "Export stored in dist/omeuka.tar.gz"
