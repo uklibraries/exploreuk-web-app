b1850c670444f2825a0ef5203f36e98bbe85e5c4
Neal <neal.powers@outlook.com>
2025-07-10 16:56:52 -0400

    change sql container to use env specific values and fix load-from-backup failing

--- utils/load-from-backup.sh
+++ utils/load-from-backup.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Check to make sure all environment variables are set
-if [ -z "$LOCAL_SQL_FILE" ] || [ -z $LOCAL_FILES_DIR ] || [ -z $DB_CONTAINER ] || [ -z $OMEKA_CONTAINER ] || [ -z $DB_USER ]; then
+if [ -z "$LOCAL_SQL_FILE" ] || [ -z $LOCAL_FILES_DIR ] || [ -z $DB_CONTAINER ] || [ -z $OMEKA_CONTAINER ]; then
 	echo "One or more environment variables not set"
-	echo "Need LOCAL_SQL_FILE, LOCAL_FILES_DIR, DB_CONTAINER, OMEKA_CONTAINER, DB_USER"
+	echo "Need LOCAL_SQL_FILE, LOCAL_FILES_DIR, DB_CONTAINER, OMEKA_CONTAINER"
 	exit 1
 fi
 
@@ -21,7 +21,7 @@ fi
 
 docker cp "$LOCAL_SQL_FILE" "$DB_CONTAINER:/tmp/backup.sql"
 
-docker exec -it $DB_CONTAINER sh -c "mysql -u $DB_USER -p omeka < /tmp/backup.sql"
+docker exec -it $DB_CONTAINER sh -c "mysql -u root -p omeka < /tmp/backup.sql"
 
 docker cp "$LOCAL_FILES_DIR/." "$OMEKA_CONTAINER:/omeka/files/"
 
