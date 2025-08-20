bfdb9e83b0a5cdf5c673f69ffc539362990f4d76
MLE Slone <m.slone@gmail.com>
2025-08-20 10:00:21 -0400

    Rename variable in prep for containerization.

--- app/exe/build.sh
+++ app/exe/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-THEME=omeukaprologue
+OMEKA_THEME=omeukaprologue
 
 bash exe/minify.sh
 
 rm -rf dist
-mkdir -p "dist/pack/themes/$THEME"
+mkdir -p "dist/pack/themes/$OMEKA_THEME"
 rsync -crlpt favicon/ dist/pack/
 rsync -crlpt shim/ dist/pack/
-rsync -crlpt theme/ "dist/pack/themes/$THEME/"
+rsync -crlpt theme/ "dist/pack/themes/$OMEKA_THEME/"
 cd dist/pack
 find . -type f -name "*.swp" | xargs -n 1 --no-run-if-empty rm
-rm "themes/$THEME/assets/css/main.css"
+rm "themes/$OMEKA_THEME/assets/css/main.css"
 find . -type d -exec chmod 0755 {} \;
 find . -type f -exec chmod 0644 {} \;
 tar zcf ../omeuka.tar.gz .
