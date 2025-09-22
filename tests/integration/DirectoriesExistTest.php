8eb961ef42f00805d8728921bc90e4e3a6d9ea86
Neal <neal.powers@outlook.com>
2025-09-22 16:05:13 -0400

    add a few basic integration tests

---
+++ tests/integration/DirectoriesExistTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class DirectoriesExistTest extends TestCase
+{
+    public function testAppDirectoryExists(): void
+    {
+        $this->assertDirectoryExists('/app', 'Expected /app directory to exist in the container');
+	}
+
+	public function testOmekaDirectoryExists(): void
+	{
+		$this->assertDirectoryExists('/omeka', 'Expected /omeka directory to exist in the container');
+	}
+}
+
