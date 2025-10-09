aefc79498731bb0b894aa80030b426ea6eadcc9b
Neal <neal.powers@outlook.com>
2025-10-09 10:04:29 -0400

    Add variable for maintainability

--- utils/restore-files.sh
+++ utils/restore-files.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+CONTAINER_FILES_DIR="/app/files"
+
 # Check to make sure all environment variables are set
 if [ -z "$LOCAL_FILES_DIR" ] || [ -z "$OMEKA_CONTAINER" ]; then
 	echo "One or more environment variables not set"
@@ -14,11 +16,11 @@ if [ ! -d "$LOCAL_FILES_DIR" ]; then
 	exit 1
 fi
 
-docker cp "$LOCAL_FILES_DIR/." "$OMEKA_CONTAINER:/omeka/files/"
+docker cp "$LOCAL_FILES_DIR/." "$OMEKA_CONTAINER:$CONTAINER_FILES_DIR"
 
-echo "Setting permissions for /omeka/files to www-data"
-docker exec "$OMEKA_CONTAINER" chown -R root:www-data "/omeka/files"
-docker exec "$OMEKA_CONTAINER" find "/omeka/files" -type d -exec chmod 0775 "{}" \;
-docker exec "$OMEKA_CONTAINER" find "/omeka/files" -type f -exec chmod 0664 "{}" \;
+echo "Setting permissions for $CONTAINER_FILES_DIR to www-data"
+docker exec "$OMEKA_CONTAINER" chown -R root:www-data "$CONTAINER_FILES_DIR"
+docker exec "$OMEKA_CONTAINER" find "$CONTAINER_FILES_DIR" -type d -exec chmod 0775 "{}" \;
+docker exec "$OMEKA_CONTAINER" find "$CONTAINER_FILES_DIR" -type f -exec chmod 0664 "{}" \;
 
 echo "Load from backup complete"
