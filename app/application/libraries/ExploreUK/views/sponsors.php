877cd5d55b37c4333fba39cddea342bf838fe693
Nicole <nrsa222@g.uky.edu>
2026-02-23 17:24:26 +0000

    rework html for grid; update sponsor logos

--- app/application/libraries/ExploreUK/views/sponsors.php
+++ app/application/libraries/ExploreUK/views/sponsors.php
@@ -1,24 +1,32 @@
-<div id="outer_footer_funders" class="slab__wrapper">
-    <div class="editorial">
-        <!-- TWIG INCLUDE : molecules-headline-group" -->
-        <!-- TWIG INCLUDE : @limestone/headline-group.twig" -->
-        <h2 id="headline-group660c465a3fe75" class="headline-group ">
-            <span class="headline-group__head ">
-                <!-- TWIG INCLUDE : @limestone/link.twig" -->
-                <span class="underline-link">Supporting organizations</span>
-                <!-- END TWIG INCLUDE : @limestone/link.twig" -->
-            </span>
-            <span class="headline-group__sub">Content made available with the support of these organizations</span>
-        </h2>
-        <!-- END TWIG INCLUDE : @limestone/headline-group.twig" -->
-        <!-- END TWIG INCLUDE : molecules-headline-group" -->
+<div class="slab slab--light-gray slab--thin">
+    <div id="outer_footer_funders" class="slab__wrapper">
+        <div class="editorial">
+            <!-- TWIG INCLUDE : molecules-headline-group" -->
+            <!-- TWIG INCLUDE : @limestone/headline-group.twig" -->
+            <h2 id="headline-group660c465a3fe75" class="headline-group ">
+                <span class="headline-group__head">Supporting Organizations</span>
+                <span class="headline-group__sub">Content made available with the support of these organizations</span>
+            </h2>
+            <!-- END TWIG INCLUDE : @limestone/headline-group.twig" -->
+            <!-- END TWIG INCLUDE : molecules-headline-group" -->
+        </div>
+        <div class="grid grid--5-up">
+            <div class="grid__column">
+                <a href="https://www.imls.gov/" target="_blank" rel="noopener"><img id="footer_funder_image" class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/imls.png') ?>" alt="Institute of Museum and Library Services" /></a>
+            </div>
+            <div class="grid__column">
+                <a href="https://www.clir.org/" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/clir.svg') ?>" alt="Council on Library and Information Resources"/></a>
+            </div>
+            <div class="grid__column">    
+                <a href="https://www.neh.gov/" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/neh.png') ?>" alt="National Endowment for the Humanities" /></a>
+            </div>
+            <div class="grid__column">    
+                <a href="https://www.archives.gov/nhprc" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/nhprc-logo.png') ?>" alt="National Archives - National Historical Publications and Records Commission"/></a>
+            </div>
+            <div class="grid__column">    
+                <a href="https://heyburncollections.org/" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/heyburn.avif') ?>" alt="The John G. Heyburn II Initiative for Excellence in the Federal Judiciary"/></a>
+            </div>
+        </div>
     </div>
-    <ul id="inner_footer_funders" class="grid grid--5-up">
-        <li><a href="https://www.imls.gov/" target="_blank" rel="noopener"><img id="footer_funder_image" class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/imls-n.png') ?>" alt="Institute of Museum and Library Services" width="231" height="83"/></a></li>
-        <li><a href="https://www.clir.org/" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/clir-n.png') ?>" alt="Council on Library and Information Resources" width="167" height="76"/></a></li>
-        <li><a href="https://www.neh.gov/" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/neh-n.jpg') ?>" alt="National Endowment for the Humanities" width="214" height="99"/></a></li>
-        <li><a href="https://www.archives.gov/nhprc" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/nhprc-n.jpg') ?>" alt="National Archives - National Historical Publications and Records Commission" width="117" height="147"/></a></li>
-        <li><a href="https://heyburncollections.org/" target="_blank" rel="noopener"><img class="lazy" src="<?= $this->themePath('images/middlegray.png') ?>" data-src="<?= $this->themePath('images/heyburn-n.png') ?>" alt="The John G. Heyburn II Initiative for Excellence in the Federal Judiciary" width="118" height="89"/></a></li>
-    </ul>
 </div>
 
