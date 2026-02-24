f3152cd23292f21dc19814f84b2363f607eed33f
Nicole <nrsa222@g.uky.edu>
2026-02-24 20:35:30 +0000

    add teaser component style

--- app/application/libraries/ExploreUK/views/front-page.php
+++ app/application/libraries/ExploreUK/views/front-page.php
@@ -5,13 +5,9 @@
 <div class="slab">
     <div class="slab__wrapper">
         <div class="editorial">
-            <!-- TWIG INCLUDE : molecules-headline-group" -->
-            <!-- TWIG INCLUDE : @limestone/headline-group.twig" -->
             <h2 class="headline-group">
                 <span class="headline-group__head ">Popular Resources</span>
             </h2>
-            <!-- END TWIG INCLUDE : @limestone/headline-group.twig" -->
-            <!-- END TWIG INCLUDE : molecules-headline-group" -->
         </div>
         <ul class="grid grid--4-up ">
             <?php foreach ($m['popular_resources'] as $index => $resource) : ?>
@@ -38,44 +34,42 @@
     </div>
 </div>
 
-<!-- END TWIG INCLUDE : @limestone/grid.twig" -->
-<div class="slab slab--blue-gray"  >
+<div class="slab slab--midnight">
     <div class="slab__wrapper">
         <div class="editorial">
-            <!-- TWIG INCLUDE : molecules-headline-group" -->
-            <!-- TWIG INCLUDE : @limestone/headline-group.twig" -->
-            <h2 class="headline-group ">
+            <h2 class="headline-group">
                 <span class="headline-group__head ">Additional Resources</span>
             </h2>
-            <!-- END TWIG INCLUDE : @limestone/headline-group.twig" -->
-            <!-- END TWIG INCLUDE : molecules-headline-group" -->
         </div>
-        <ul class="grid grid--3-up ">
+        <div class="grid grid--3-up">
             <?php foreach ($m['additional_resources'] as $index => $resource) : ?>
-                <li class="grid__column">
-                    <a
-                        aria-label="<?= $resource['label'] ?>"
-                        id="additional-resource-<?= $index ?>"
-                        href="<?= $resource['url'] ?>"
-                        target="_blank"
-                        rel="noopener"
-                    >
-                        <img
-                            class="lazy"
-                            src="<?= $this->themePath('images/middlegray.png') ?>"
-                            data-src="<?= $resource['image'] ?>"
-                            title="<?= $resource['label'] ?>"
-                            alt="<?= htmlspecialchars((string)$resource['label']) ?>"
-                        >
-                    </a>
-                </li>
+                <div class="grid__column">
+                    <div class="teaser--midnight">
+                        <div class="teaser__media">
+                            <a href="<?= $resource['url'] ?>">
+                                <img src="<?= $resource['image'] ?>" alt="<?= htmlspecialchars((string)$resource['label']) ?>"/>
+                            </a>
+                        </div>
+                        <div class="teaser__content">
+                            <h3 class="headline-group ">
+                                <span class="headline-group__head">
+                                    <a href="<?= $resource['url'] ?>" class="underline-link" id="additional-resource-<?= $index ?>">
+                                        <?= $resource['label'] ?>
+                                    </a>
+                                </span>
+                            </h3>
+                            <div class="editorial">
+					            <p>SPOKEdb currently provides access to 19892 interview records, and 765 projects.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <?php endforeach; ?>
-        </ul>
+        </div>
     </div>
 </div>
-<!-- END TWIG INCLUDE : @limestone/grid.twig" -->
 
-
+<?php //require('additional-resources.php'); ?>
 <?php require('sponsors.php'); ?>
 <?php require('global-footer.php'); ?>
 <?php require('universal-footer.php'); ?>
