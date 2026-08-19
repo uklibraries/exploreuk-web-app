5f91cb53018a289bd7bc54da0688fe48ff80ab60
Nicole <nrsa222@g.uky.edu>
2026-08-19 22:56:25 +0000

    adjust header and breadcrumb pattern to match FA frontend styling

--- app/application/libraries/ExploreUK/views/breadcrumbs.php
+++ app/application/libraries/ExploreUK/views/breadcrumbs.php
@@ -1,19 +1,23 @@
-<nav class="breadcrumbs">
-    <ul class="no-decoration">
-        <li>
-            <!-- TWIG INCLUDE : @limestone/link.twig" -->
-            <a href="<?= $this->path('') ?>" class="">Home</a>
-            <!-- END TWIG INCLUDE : @limestone/link.twig" -->
-        </li>
-        <?php if (isset($m['back_to_search']) && isset($m['back_to_search_text'])) : ?>
-            <li>
-                <!-- TWIG INCLUDE : @limestone/link.twig" -->
-                <a href="<?= $m['back_to_search'] ?>" class=""><?= $m['back_to_search_text'] ?></a>
-                <!-- END TWIG INCLUDE : @limestone/link.twig" -->
-            </li>
-        <?php endif; ?>
-        <?php if (isset($m['current_page_title'])) : ?>
-            <li aria-current="page"><?= $m['current_page_title'] ?></li>
-        <?php endif; ?>
-    </ul>
-</nav>
+<div class="slab slab--thin slab--wildcat-white">
+    <div class="slab__wrapper">
+        <nav class="breadcrumbs">
+            <ul class="no-decoration">
+                <li>
+                    <!-- TWIG INCLUDE : @limestone/link.twig" -->
+                    <a href="<?= $this->path('') ?>" class="">Home</a>
+                    <!-- END TWIG INCLUDE : @limestone/link.twig" -->
+                </li>
+                <?php if (isset($m['back_to_search']) && isset($m['back_to_search_text'])) : ?>
+                    <li>
+                        <!-- TWIG INCLUDE : @limestone/link.twig" -->
+                        <a href="<?= $m['back_to_search'] ?>" class=""><?= $m['back_to_search_text'] ?></a>
+                        <!-- END TWIG INCLUDE : @limestone/link.twig" -->
+                    </li>
+                <?php endif; ?>
+                <?php if (isset($m['current_page_title'])) : ?>
+                    <li aria-current="page"><?= $m['current_page_title'] ?></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </div>
+</div>
