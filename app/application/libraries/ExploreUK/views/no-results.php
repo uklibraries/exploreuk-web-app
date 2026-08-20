f910741de0015710ba0f422729d3ea02c52e87d7
Nicole <nrsa222@g.uky.edu>
2026-08-20 17:35:30 +0000

    cleanup html

--- app/application/libraries/ExploreUK/views/no-results.php
+++ app/application/libraries/ExploreUK/views/no-results.php
@@ -16,23 +16,21 @@
     <div class="slab">
         <div class="slab__wrapper">
             <div class="editorial">
-                <div class="result-summary">
-                    <h2>No results found matching your search for '<?= htmlspecialchars((string) $this->q('q'), ENT_QUOTES, 'UTF-8') ?>'.</h2>
-                    <p>
-                        Did you mean:
-                        <?php
-                        $suggestions = $m['suggestions'];
-                        foreach ($suggestions as $index => $suggestion) {
-                            $link = $this->suggestedLink($suggestion);
-                            echo '<a class="suggested-search" href="' . $link . '">' . $suggestion . '</a>';
-                            if ($index + 1 < count($suggestions)) {
-                                echo ' or ';
-                            }
+                <h2>No results found matching your search for '<?= htmlspecialchars((string) $this->q('q'), ENT_QUOTES, 'UTF-8') ?>'.</h2>
+                <p>
+                    Did you mean:
+                    <?php
+                    $suggestions = $m['suggestions'];
+                    foreach ($suggestions as $index => $suggestion) {
+                        $link = $this->suggestedLink($suggestion);
+                        echo '<a class="suggested-search" href="' . $link . '">' . $suggestion . '</a>';
+                        if ($index + 1 < count($suggestions)) {
+                            echo ' or ';
                         }
-                        ?>
-                        ?
-                    </p>
-                </div>
+                    }
+                    ?>
+                    ?
+                </p>
             </div>
         </div>
     </div>
