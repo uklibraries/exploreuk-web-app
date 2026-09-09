f73674fbbb28f5f1a60216aa34926f60d2bb9632
Neal <neal.powers@outlook.com>
2026-09-09 11:10:55 -0400

    point rector to current PHP version; fixes #141

--- rector.php
+++ rector.php
@@ -2,17 +2,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 
 return RectorConfig::configure()
 	->withPaths([
 	__DIR__ . '/app/shim',
 ])
-// uncomment to reach your current PHP version
-//->withPhpSets()
-->withSets([
-	LevelSetList::UP_TO_PHP_84,
-])
+# Ensures Rector runs with whatever PHP version it finds in composer.json
+->withPhpSets()
 ->withComposerBased(phpunit: true)
 ->withTypeCoverageLevel(0)
 ->withDeadCodeLevel(0)
