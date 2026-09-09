b3ee90bac053c1074e78b2f7be3220b0f75f893a
Neal <neal.powers@outlook.com>
2026-09-09 11:11:54 -0400

    Point rector to new paths; fixes #140

--- rector.php
+++ rector.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-	->withPaths([
-	__DIR__ . '/app/shim',
-])
+    ->withPaths([
+        __DIR__ . '/app/catalog.php',
+        __DIR__ . '/app/application/libraries/ExploreUK',
+        __DIR__ . '/tests',
+    ])
+
 # Ensures Rector runs with whatever PHP version it finds in composer.json
 ->withPhpSets()
 ->withComposerBased(phpunit: true)
